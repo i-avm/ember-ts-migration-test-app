1271465862e9ba8227f9df3aa08fd5f683b87888
Abin Varghese <abin.varghese@mitsogo.com>
2022-11-15 19:47:43 +0530

    run ember install ember-cli-ts

---
+++ types/global.d.ts
@@ -0,0 +1,7 @@
+// Types for compiled templates
+declare module 'ember-app-on-48/templates/*' {
+  import { TemplateFactory } from 'ember-cli-htmlbars';
+
+  const tmpl: TemplateFactory;
+  export default tmpl;
+}
